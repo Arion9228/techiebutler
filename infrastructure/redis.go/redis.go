8743f95d9e3da8539a3502c460a4d4808d5233ba
Madhav Sharma <madhavsharma@Madhavs-MacBook-Pro.local>
2024-05-07 17:58:47 +0530

    added redis

---
+++ infrastructure/redis.go/redis.go
@@ -0,0 +1,38 @@
+package redisUtil
+
+import (
+	"context"
+	"log"
+
+	redis "github.com/go-redis/redis/v8"
+)
+
+func NewClient() *redis.Client {
+	// Create a new Redis client
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Redis server address
+		Password: "",               // No password set
+		DB:       0,                // Use the default database
+	})
+	err := Ping(client)
+	if err != nil {
+		log.Fatal(err.Error())
+		return nil
+	}
+
+	return client
+}
+
+// Ping checks if the Redis server is reachable.
+func Ping(client *redis.Client) error {
+	// Ping the Redis server to check if it's reachable
+	ctx := context.Background()
+	pong, err := client.Ping(ctx).Result()
+	if err != nil {
+		return err
+	}
+	if pong != "PONG" {
+		return redis.Nil
+	}
+	return nil
+}
