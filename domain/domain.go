72eb5c87f38c3f0e7f19487b6b1c63a72eb5346f
Madhav Sharma <madhavsharma@Madhavs-MacBook-Pro.local>
2024-05-07 14:58:07 +0530

    added employee interface

--- domain/domain.go
+++ domain/domain.go
@@ -6,3 +6,10 @@ type Employee struct {
 	Position string  `json:"position"`
 	Salary   float64 `json:"salary"`
 }
+
+type EmployeeRepository interface {
+	CreateEmployee(employee Employee) error
+	GetEmployeeByID(id int) (Employee, error)
+	UpdateEmployee(employee Employee) error
+	DeleteEmployee(id int) error
+}
